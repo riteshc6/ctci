6932e68b91b8573f5da22db3be6c90187a692636
Ritesh Chaurasia <riteshc6@gmail.com>
2020-01-15 15:28:28 +0530

    URLify

---
+++ data_structures/1_arrays_strings/URLify.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+
+def URLify(string, length):
+
+    string = string.rstrip()    
+    string = string.replace(" ", "%20")
+    return string
+
+
+
+class Test(unittest.TestCase):
+
+    def test_urlify(self):
+        data = [
+            ('much ado about nothing      ', 22,
+            'much%20ado%20about%20nothing'),
+            ('Mr John Smith    ', 13, 'Mr%20John%20Smith')]
+
+        for url, length, expected_url in data:
+            URLified = URLify(url, length)
+            self.assertEqual(URLified, expected_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
