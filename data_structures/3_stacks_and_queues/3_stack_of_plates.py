1c344e2c1fbda39259cd553d585ed2891d136622
Ritesh Chaurasia <riteshc6@gmail.com>
2020-02-12 10:30:27 +0530

    3 popAt array implementation

--- data_structures/3_stacks_and_queues/3_stack_of_plates.py
+++ data_structures/3_stacks_and_queues/3_stack_of_plates.py
@@ -22,17 +22,31 @@ class SetOfStacks:
         if self.is_empty():
             raise Exception("Stack is empty")
         data = self.values.pop(-1)
+        self.update_stacks_size()
+        return data
+    
+    def update_stacks_size(self):
         if self.stacks_size[-1] == 1:
             self.stacks_size.pop(-1)
         else:
             self.stacks_size[-1] -= 1
-        return data
-    
+
     def is_empty(self):
         return not len(self.stacks_size)
     
     def is_stack_full(self):
         return self.stacks_size[-1] == self.threesold
+
+    def popAt(self, stack_index):
+        values_index = self.get_data_index(stack_index)
+        data = self.values.pop(values_index)
+        self.update_stacks_size()
+        return data
+    
+    def get_data_index(self, stack_index):
+        offset = (stack_index * self.threesold) - 1
+        return offset + self.stacks_size[stack_index]
+
 
 stacks = SetOfStacks(3)
 data = [3,4,5,7,10,12,14,1]
@@ -40,7 +54,15 @@ for value in data:
     stacks.push(value)
     print(stacks)
 
-for _ in range(len(data)):
-    stacks.pop()
-    print(stacks)
+stacks.popAt(1)
+print(stacks)
+stacks.popAt(1)
+print(stacks)
+stacks.popAt(0)
+print(stacks)
+
+# for _ in range(len(data)):
+#     stacks.pop()
+#     print(stacks)
+
 
