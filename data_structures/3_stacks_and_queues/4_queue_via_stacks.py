cf2c5c5036c412ce63cb2ad652e4858fa495a370
Ritesh Chaurasia <riteshc6@gmail.com>
2020-02-13 09:20:11 +0530

    4 queue via stacks

---
+++ data_structures/3_stacks_and_queues/4_queue_via_stacks.py
@@ -0,0 +1,63 @@
+from stack_ll import Stack
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = Stack()
+        self.s2 = Stack()
+    
+    def __str__(self):
+        string = "S1 -> "
+        cur1 = self.s1.top
+        while cur1:
+            string += str(cur1.data) + ","
+            cur1 = cur1.next
+        string += "--- S2 -> "
+        cur2 = self.s2.top
+        while cur2:
+            string += str(cur2.data)
+            cur2 = cur2.next
+        return string
+
+
+    def push(self, data):
+        self.s1.push(data)
+    
+    def pop(self):
+        if self.s2.is_empty():
+            self.reverse_stack()
+        data = self.s2.pop()
+        return data
+    
+    def peek(self):
+        if self.s2.is_empty():
+            self.reverse_stack()
+        data = self.s2.peek()
+        return data
+
+    def reverse_stack(self):
+        # Pop s1 and add it in reverse order to s2
+        while not self.s1.is_empty():
+            data = self.s1.pop()
+            self.s2.push(data)
+
+queue = MyQueue()
+values = [1, 2, 3, 4]
+for value in values:
+    queue.push(value)
+print(queue)
+
+queue.pop()
+print(queue)
+queue.pop()
+print(queue)
+queue.push(8)
+print(queue)
+queue.push(10)
+print(queue)
+queue.pop()
+print(queue)
+queue.pop()
+print(queue)
+queue.peek()
+print(queue)
