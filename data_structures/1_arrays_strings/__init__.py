2dbd56fa08cb1a0dd3d3917f63d862ad38eb1533
Ritesh Chaurasia <riteshc6@gmail.com>
2020-02-14 11:25:37 +0530

    added hash table data structure

--- data_structures/1_arrays_strings/__init__.py
+++ data_structures/1_arrays_strings/__init__.py
@@ -1,4 +1,42 @@
 """
-    Since I know the basics of hashmap and it's implementations so I decided to just go through python specific
-    dict. This chapter topics are too java specific like arraylist, stringbuilder so I'm skipping those
+    Hash Table Implementation using built-in deque of collections
 """
+from collections import deque
+
+class HashTable:
+
+    def __init__(self, table_size):
+        self.table_size = table_size
+        self.array = [deque() for _ in range(table_size)]
+
+    def __str__(self):
+        return str(self.array)
+
+
+    def insert_data(self, data):
+        key = self.get_hash(data)
+        self.array[key].append(data)
+
+    def get_hash(self, data):
+        return hash(data) % self.table_size
+        
+
+    def is_present(self, data):
+        key = self.get_hash(data)
+        try:
+            index = self.array[key].index(data)
+            return True
+        except ValueError:
+            return False
+
+
+
+lookup = HashTable(5)
+
+values = [1,2,3,4,5,5,6,6,7,7,8,3,4]
+
+for value in values:
+    lookup.insert_data(value)
+
+print(lookup)
+print(lookup.is_present(7))
