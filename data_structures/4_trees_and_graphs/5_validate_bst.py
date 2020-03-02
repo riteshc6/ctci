77093baca87c058c047191162f10da062b3e64ce
Ritesh Chaurasia <riteshc6@gmail.com>
2020-03-02 23:23:45 +0530

    5 validate bst: added correct bst algo

--- data_structures/4_trees_and_graphs/5_validate_bst.py
+++ data_structures/4_trees_and_graphs/5_validate_bst.py
@@ -3,7 +3,9 @@ from binary_tree import Node as b_node
 
 def is_bst(tree: object) -> bool:
     """
-        checks if a  given binary tree is a binary search tree
+        checks if a  given binary tree is a binary search tree based on left_node < node < right_node
+        WARNING: This algo doesn't checks if all nodes on left are lesser than the current node and
+                 if  all nodes on right are greater than the current node
     """
     if tree:
 
@@ -31,6 +33,33 @@ def is_bst(tree: object) -> bool:
         
         return True
 
+def is_correct_bst(tree):
+    """
+        Completely checks if all of left_nodes < node < all of right_nodes
+    """
+    return check_bst(tree, None, None)
+
+def check_bst(node, min_, max_):
+    """
+        Recursively checks all nodes
+    """
+    if not node:
+        return True
+    
+    if min_:
+        if node.data <= min_:
+            return False        
+    
+    if max_:
+        if node.data > max_:
+            return False
+    
+    if (not check_bst(node.left, min_, node.data)) or (not check_bst(node.right, node.data, max_)):
+        return False
+    
+    return True
+
+
 
 if __name__ == "__main__":
     tree = Node()
@@ -49,4 +78,10 @@ if __name__ == "__main__":
     b_tree.right.right = b_node(20)
     print(is_bst(b_tree))
     
+    bug = b_node(20)
+    bug.left = b_node(10)
+    bug.left.right = b_node(25)
+    bug.right = b_node(30)
+    print(is_bst(bug))
+    print(is_correct_bst(bug))
     
