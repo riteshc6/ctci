39d32d08827d02c3622cdb62b71ba504445f4541
Ritesh Chaurasia <riteshc6@gmail.com>
2020-05-04 08:48:39 +0530

    technical_questions:common_elements-> updated logic

--- technical_questions/common_elements.py
+++ technical_questions/common_elements.py
@@ -1,18 +1,17 @@
-array1 = [13, 27, 35, 40, 49, 55, 59]
-array2 = [17, 35, 39, 40, 55, 58, 60]
+A = [13, 27, 35, 40, 49, 55, 59]
+B = [17, 35, 39, 40, 55, 58, 60]
 
+length_b = len(B)
 i = 0
 count = 0
-for array1_elem in array1:
-    
-    for j in range(i, len(array2)):
 
-        if array1_elem < array2[j]:
-            i = j
-            break
-        elif array1_elem == array2[j]:
+for elem in A:
+    while i < length_b and B[i] <= elem:
+        if elem == B[i]:
             count += 1
-            i = j
+            i += 1
             break
+        i += 1
 
 print(count)
+
