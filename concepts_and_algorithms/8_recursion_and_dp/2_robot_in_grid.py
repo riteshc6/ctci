be9e66b699d64a8b6356f4ac5097a50bb86dab61
Ritesh Chaurasia <riteshc6@gmail.com>
2020-07-06 13:37:00 +0530

    8->2_robot_grid with memoization

--- concepts_and_algorithms/8_recursion_and_dp/2_robot_in_grid.py
+++ concepts_and_algorithms/8_recursion_and_dp/2_robot_in_grid.py
@@ -31,6 +31,38 @@ def _get_path(maze:list, row: int, col: int, path: list, failed_points: set):
     failed_points.add(p)
     return False
 
+# Memoized solution
+def get_path_memo(maze:list):
+    last_row = len(maze) - 1
+    last_col = len(maze[0]) - 1
+    path = []
+    cache = {}
+    if _get_path_memo(maze, last_row, last_col, path, cache):
+        return path
+    return None
+
+def _get_path_memo(maze, row, col, path, cache):
+    if row < 0 or col < 0 or not maze[row][col]:
+        return False
+    
+    p = (row, col)
+
+    if p in cache:
+        return cache[p]
+    
+    isAtOrigin = (row == 0) and (col == 0)
+    success = False
+
+    if isAtOrigin or _get_path_memo(maze, row - 1, col, path, cache) \
+        or _get_path(maze, row, col - 1, path, cache):
+        success = True
+        path.append(p)
+
+    cache[p] = success
+    return success
+
 maze = [[True, True], [False, True]]
 print(get_path(maze))
+print(get_path([[True, True, False],[True,True, True], [False, True, True]]))
+print(get_path_memo([[True, True, False],[True,True, True], [False, True, True]]))
 
