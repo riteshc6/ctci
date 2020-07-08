69184509e19c1eb0c61ece575c789b789b677650
Ritesh Chaurasia <riteshc6@gmail.com>
2020-07-08 22:24:52 +0530

    8-> 7_permutation_with_duplicates

--- concepts_and_algorithms/8_recursion_and_dp/7_permutation_with_dups.py
+++ concepts_and_algorithms/8_recursion_and_dp/7_permutation_with_dups.py
@@ -11,3 +11,42 @@ def get_permutations(string: str):
     return new_perms
 
 print(get_permutations("abcd"))
+
+def permutations_using_prefix(string: str):
+    result = []
+    _permutations_using_prefix("", string, result)
+    return result
+
+def _permutations_using_prefix(prefix: str, remainder: str, result: list):
+    if len(remainder) == 0: result.append(prefix)
+
+    length = len(remainder)
+    for i in range(length):
+        before = remainder[:i]
+        after = remainder[i + 1:]
+        c = remainder[i]
+        _permutations_using_prefix(prefix + c, before + after, result)
+
+
+print(permutations_using_prefix("abc"))
+
+
+def get_perms(remainder: str):
+    length = len(remainder)
+    result = []
+    if length == 0:
+        result.append("")
+        return result
+    
+    for i in range(length):
+        before = remainder[0: i]
+        after = remainder[i + 1:]
+
+        partials = get_perms(before + after)
+
+        for word in partials:
+            result.append(remainder[i] + word)
+        
+    return result
+
+print(get_perms("abd"))
