e0db24f4a4372d6e9662ee1b97e80f47ceaa5841
Ritesh Chaurasia <riteshc6@gmail.com>
2020-07-10 08:05:47 +0530

    8-> 9_parens more efficient solution

--- concepts_and_algorithms/8_recursion_and_dp/9_parens.py
+++ concepts_and_algorithms/8_recursion_and_dp/9_parens.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def gen_parens(remaining: int):
     parens = set()
     if remaining == 0:
@@ -14,3 +16,28 @@ def gen_parens(remaining: int):
     return parens
 
 print(gen_parens(3))
+
+
+def add_paren(parens_list: List[str], left_rem: int, right_rem: int, string_list: List[str], count: int):
+    if left_rem < 0 or right_rem < left_rem:
+        return
+    
+    if left_rem == 0 and right_rem == 0:
+        string = "".join(string_list)
+        parens_list.append(string)
+    else:
+        if left_rem > 0:
+            string_list[count] = "("
+            add_paren(parens_list, left_rem - 1, right_rem, string_list, count + 1)
+        
+        if right_rem > left_rem:
+            string_list[count] = ")"
+            add_paren(parens_list, left_rem, right_rem - 1, string_list, count + 1)
+
+def generate_parens(count: int):
+    string_list = [0] * count * 2
+    parens_list = []
+    add_paren(parens_list, count, count, string_list, 0)
+    return parens_list
+
+print(generate_parens(3))
